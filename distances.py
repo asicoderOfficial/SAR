868a699d9cd3c9db1efdc7530b56aef623412ed3
asicoderofficial@gmail.com <asicoderofficial@gmail.com>
2021-10-02 18:27:46 +0200

    Matriz acabada

--- distances.py
+++ distances.py
@@ -6,7 +6,7 @@ def crear_matriz_levenshtein(begin, end):
     Dadas 2 strings, la inicial (begin) y en la que se quiere convertir (end),
     devolver la matriz necesaria para calcular la distancia de Levenshtein.
     Ejemplo -> para begin=benyam y end=ephrem, se genera:
-    [[0. 0. 0. 0. 0. 0. 0.]
+    [[0. 1. 2. 3. 4. 5. 6.]
      [1. 0. 0. 0. 0. 0. 0.]
      [2. 0. 0. 0. 0. 0. 0.]
      [3. 0. 0. 0. 0. 0. 0.]
@@ -16,7 +16,6 @@ def crear_matriz_levenshtein(begin, end):
     """
     levmatrix = np.zeros((len(end)+1, len(begin)+1))
     levmatrix[:,0] = np.asarray(list(range(len(end)+1)))
+    levmatrix[0,:] = np.asarray(list(range(len(begin)+1)))
     return levmatrix
-
-
 
