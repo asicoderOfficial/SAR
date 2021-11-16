20366cc31c55540b23920fed6020929bf8512767
Javier <None>
2021-11-16 19:48:15 +0100

    PA TI

--- spellsuggest.py
+++ spellsuggest.py
@@ -3,14 +3,16 @@ import re
 from distances import levenshtein_optimized_restinged, dp_restricted_damerau_threshold, dp_intermediate_damerau_threshold
 from levels import level_flat
 from trie import Trie
-
+import collections
+import time
+import numpy as np
 class SpellSuggester:
 
     """
     Clase que implementa el método suggest para la búsqueda de términos.
     """
 
-    def __init__(self, vocab_file_path):
+    def __init__(self, vocab_file_path,vocab=None):
         """Método constructor de la clase SpellSuggester
 
         Construye una lista de términos únicos (vocabulario),
@@ -20,8 +22,10 @@ class SpellSuggester:
             vocab_file (str): ruta del fichero de texto para cargar el vocabulario.
 
         """
-
-        self.vocabulary  = self.build_vocab(vocab_file_path, tokenizer=re.compile("\W+"))
+        if vocab is not None:
+            self.vocabulary = vocab
+        else:
+            self.vocabulary  = self.build_vocab(vocab_file_path, tokenizer=re.compile("\W+"))
 
     def build_vocab(self, vocab_file_path, tokenizer):
         """Método para crear el vocabulario.
@@ -67,7 +71,7 @@ class SpellSuggester:
         
         for w in self.vocabulary:
             if (distance == "levensthein"):
-                if(level_flat(term,w) <= threshold)
+                if(level_flat(term,w) <= threshold):
                     if (abs(len(w)-lengword) <= threshold):
                         MetDist[distance](term,w, threshold)
                         if (Dist <= threshold and Dist != None):
@@ -87,30 +91,30 @@ class TrieSpellSuggester(SpellSuggester):
             M1 = np.zeros(a)
             M2 = np.zeros(a)
             for i in range(1,a):
-                V1[i]= M1[self.trie.get_parent(i)] + 1
+                M1[i]= M1[self.trie.get_parent(i)] + 1
 
             for col in range(1,b + 1):
-                V2[0]=col
+                M2[0]=col
                 for fil in range(1,a) :
                     cost = not term[col-1] == self.trie.get_label(fil)
-                    V2[fil] = min(V1[fil] + 1,
-                                V2[self.trie.get_parent(fil)] + 1,
-                                V1[self.trie.get_parent(fil)] + cost)
+                    M2[fil] = min(M1[fil] + 1,
+                                M2[self.trie.get_parent(fil)] + 1,
+                                M1[self.trie.get_parent(fil)] + cost)
                 if min(M2) > threshold:
                      return {}
                 M1, M2 = M2, M1
 
-            for i in range(n):
+            for i in range(a):
                 if self.trie.is_final(i):
-                    if V1[i] <= threshold: results[self.trie.get_output(i)] = V1[i]
+                    if M1[i] <= threshold: results[self.trie.get_output(i)] = M1[i]
             return results
         else: return super().suggest(term, distance, threshold)
 
     """
     Clase que implementa el método suggest para la búsqueda de términos y añade el trie
     """
-    def __init__(self, vocab_file_path):
-        super().__init__(vocab_file_path)
+    def __init__(self, vocab_file_path,vocab=None):
+        super().__init__(vocab_file_path,vocab)
         self.trie = Trie(self.vocabulary)
     
 if __name__ == "__main__":
@@ -118,17 +122,26 @@ if __name__ == "__main__":
     tam = (2500,5000, 10000, 15000, 20000, 25000, 30000, 35000)
     th = (1, 2, 3, 4, 5, 7)
     m = ("intermediate", "restricted", "levenshtein")
+    vocab_file_path = "./quijote.txt"
+    tokenizer = re.compile("\W+")
+    with open(vocab_file_path, "r", encoding='utf-8') as fr:
+        c = collections.Counter(tokenizer.split(fr.read().lower()))
+    if '' in c:
+        del c['']
+    reversed_c = [(freq, word) for (word, freq) in c.items()]
+    sorted_reversed = sorted(reversed_c, reverse=True)
+    sorted_vocab = sorted([word for (freq, word) in sorted_reversed])
     for p in pal:
         print(p)
         for x in tam: 
-            spellsuggester = SpellSuggester("./corpora/quijote.txt", x)    
+            spellsuggester = SpellSuggester("./quijote.txt", sorted_vocab[:x])
             for z in m:
                 for y in th:
                     tini = time.process_time()
                     spellsuggester.suggest(p, z, threshold = y)
                     t = time.process_time() - tini
                     print('Trie: No   Tamaño: '+ str(x) + '   Threshold: ' + str(y) + '   Método: ' + z + '   :   ' + str(t))
-            spellsuggester = TrieSpellSuggester("./corpora/quijote.txt", x)
+            spellsuggester = TrieSpellSuggester("./quijote.txt", sorted_vocab[:x])
             for y in th:
                 tini = time.process_time()
                 spellsuggester.suggest(p, "levenshtein", threshold = y)
