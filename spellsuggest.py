bc9c43c5f48cf782f011c9f1cb34f40b43e4ff25
Rodrigo Juanes Busolo <rodjuabu@gmail.com>
2021-11-16 19:03:00 +0100

    Spellsugest con trie incorporado y medición de tiempos

--- spellsuggest.py
+++ spellsuggest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
-from distances import levenshtein_optimized_restinged, dp_restricted_damerau_iterative, dp_intermediate_damerau_threshold
+from distances import levenshtein_optimized_restinged, dp_restricted_damerau_threshold, dp_intermediate_damerau_threshold
 from levels import level_flat
 from trie import Trie
 
@@ -54,14 +54,14 @@ class SpellSuggester:
                 o filtrando la salida de las distancias de la tarea 2
         """
         assert distance in ["levenshtein", "restricted", "intermediate"]
-
+        if threshold == None: threshold = 2**31
         results = {} # diccionario termino:distancia
         lengword = len(term) #Agilizar dentro del bucle.
         MetDist = {}
         if (distance == "levensthein"):
             MetDist[distance] = levenshtein_optimized_restinged
         elif (distance == "restricted" ):
-            MetDist[distance] = dp_restricted_damerau_iterative
+            MetDist[distance] = dp_restricted_damerau_threshold
         elif (distance == "intermediate"):
             MetDist[distance] = dp_intermediate_damerau_threshold
         
@@ -71,14 +71,41 @@ class SpellSuggester:
                     if (abs(len(w)-lengword) <= threshold):
                         MetDist[distance](term,w, threshold)
                         if (Dist <= threshold and Dist != None):
-                        #No estoy seguro de si esto está ok, pero implemento diccionario para --> distancia:[word]}
-                            if (Dist not in results):
-                                results[Dist] = [w]
-                            else:
-                                results[Dist].append(w)
+                        #No estoy seguro de si esto está ok, pero implemento diccionario para --> word:distancia}
+                            if (w not in results):
+                                results[w] = Dist
         return results
 
 class TrieSpellSuggester(SpellSuggester):
+    def suggest(self, term, distance="levenshtein", threshold=None):
+        if distance == "levenshtein":
+            results = {}
+            if threshold == None: 
+                threshold = 2**31
+            a = self.trie.get_num_states()
+            b = len(term)
+            M1 = np.zeros(a)
+            M2 = np.zeros(a)
+            for i in range(1,a):
+                V1[i]= M1[self.trie.get_parent(i)] + 1
+
+            for col in range(1,b + 1):
+                V2[0]=col
+                for fil in range(1,a) :
+                    cost = not term[col-1] == self.trie.get_label(fil)
+                    V2[fil] = min(V1[fil] + 1,
+                                V2[self.trie.get_parent(fil)] + 1,
+                                V1[self.trie.get_parent(fil)] + cost)
+                if min(M2) > threshold:
+                     return {}
+                M1, M2 = M2, M1
+
+            for i in range(n):
+                if self.trie.is_final(i):
+                    if V1[i] <= threshold: results[self.trie.get_output(i)] = V1[i]
+            return results
+        else: return super().suggest(term, distance, threshold)
+
     """
     Clase que implementa el método suggest para la búsqueda de términos y añade el trie
     """
@@ -87,8 +114,25 @@ class TrieSpellSuggester(SpellSuggester):
         self.trie = Trie(self.vocabulary)
     
 if __name__ == "__main__":
-    spellsuggester = TrieSpellSuggester("./corpora/quijote.txt")
-    print(spellsuggester.suggest("alábese"))
-    # cuidado, la salida es enorme print(suggester.trie)
+    pal = ("casa", "senor", "jabón", "constitución", "ancho", "savaedra", "vicios", "quixot", "s3afg4ew")
+    tam = (2500,5000, 10000, 15000, 20000, 25000, 30000, 35000)
+    th = (1, 2, 3, 4, 5, 7)
+    m = ("intermediate", "restricted", "levenshtein")
+    for p in pal:
+        print(p)
+        for x in tam: 
+            spellsuggester = SpellSuggester("./corpora/quijote.txt", x)    
+            for z in m:
+                for y in th:
+                    tini = time.process_time()
+                    spellsuggester.suggest(p, z, threshold = y)
+                    t = time.process_time() - tini
+                    print('Trie: No   Tamaño: '+ str(x) + '   Threshold: ' + str(y) + '   Método: ' + z + '   :   ' + str(t))
+            spellsuggester = TrieSpellSuggester("./corpora/quijote.txt", x)
+            for y in th:
+                tini = time.process_time()
+                spellsuggester.suggest(p, "levenshtein", threshold = y)
+                t = time.process_time() - tini
+                print('Trie: Sí   Tamaño: '+ str(x) + '   Threshold: ' + str(y) + '   Método: levenshtein   :   ' + str(t))
 
     
