a68b08b147df346744c0939fc3fb88026e9103b2
asicoderofficial@gmail.com <asicoderofficial@gmail.com>
2021-11-24 16:28:00 +0100

    ahora si las stats 2

--- medias.py
+++ medias.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('tiempos1.csv')
-df['media_trie-distancia'] = df.groupby(['trie', 'distancia'])['tiempo'].transform('mean')
-df['mediana_trie-distancia'] = df.groupby(['trie', 'distancia'])['tiempo'].transform('median')
-df.to_csv('tiempos_stats.csv')
+df['media_trie-distancia'] = df.groupby(['trie', 'distancia', 'palabra', 'tamanyo'])['tiempo'].transform('mean')
+df['mediana_trie-distancia'] = df.groupby(['trie', 'distancia', 'palabra', 'tamanyo'])['tiempo'].transform('median')
+df.to_csv('tiempos_stats2.csv')
