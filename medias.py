a4f1cef65417a635fb644e09ab1648f090aed766
asicoderofficial@gmail.com <asicoderofficial@gmail.com>
2021-11-24 16:55:31 +0100

    ahora si las stats 4

--- medias.py
+++ medias.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('tiempos1.csv')
-df['media_trie-distancia'] = df.groupby(['trie', 'distancia', 'palabra', 'tamanyo'])['tiempo'].transform('mean')
-df['mediana_trie-distancia'] = df.groupby(['trie', 'distancia', 'palabra', 'tamanyo'])['tiempo'].transform('median')
+df['media_trie-distancia'] = df.groupby(['trie', 'distancia', 'tamanyo'])['tiempo'].transform('mean')
+df['mediana_trie-distancia'] = df.groupby(['trie', 'distancia', 'tamanyo'])['tiempo'].transform('median')
 df['distancia'] = df['distancia'].str.replace('intermediate','i')
 df['distancia'] = df['distancia'].str.replace('restricted','d')
 df['distancia'] = df['distancia'].str.replace('levenshtein','l')
