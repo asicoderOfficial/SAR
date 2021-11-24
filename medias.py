b41ef5a3d340f7b2fe156a24634c170d1df018cb
asicoderofficial@gmail.com <asicoderofficial@gmail.com>
2021-11-24 19:53:57 +0100

    free fire

--- medias.py
+++ medias.py
@@ -2,8 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from os import listdir
+from os.path import isfile, join
 
-df = pd.read_csv('tiempos1.csv')
+#df = pd.read_csv('tiempos1.csv')
+"""
 df['media_trie-distancia'] = df.groupby(['trie', 'distancia', 'tamanyo', 'threshold'])['tiempo'].transform('mean')
 df['mediana_trie-distancia'] = df.groupby(['trie', 'distancia', 'tamanyo', 'threshold'])['tiempo'].transform('median')
 df['distancia'] = df['distancia'].str.replace('intermediate','i')
@@ -17,3 +20,31 @@ df2 = pd.pivot_table(df, values='media_trie-distancia', index=['tamanyo', 'dista
 print(df2)
 df2.to_csv('CSV_FINAL.csv')
 #df.to_csv('tiempos_stats4.csv')
+"""
+###############################################
+
+mypath = 'resultadosConMod/'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+df = pd.DataFrame()
+trie = []
+for f in onlyfiles:
+    curr_df = pd.read_csv(mypath + f, delimiter='\t', header=None)
+    if 'trie' in f:
+        trie += ['Yes'] * curr_df.shape[0]
+    else:
+        trie += ['No'] * curr_df.shape[0]
+    df = pd.concat([df, pd.read_csv(mypath + f, delimiter='\t', header=None)])
+df['media'] = df[4].str.split(' ').str[1]
+df['media'] = pd.to_numeric(df['media'])
+df['trie'] = trie
+df = df.rename(columns={0:'distancia', 1:'palabra', 2:'threshold', 3:'tamanyo'})
+df['media_trie-distancia'] = df.groupby(['trie', 'distancia', 'tamanyo', 'threshold'])['media'].transform('mean')
+df['distancia'] = df['distancia'].str.replace('intermediate','i')
+df['distancia'] = df['distancia'].str.replace('restricted','d')
+df['distancia'] = df['distancia'].str.replace('levenshtein','l')
+df['distancia-trie'] = df['distancia'] + df['trie']
+del df['distancia']
+del df['trie']
+df2 = pd.pivot_table(df, values='media_trie-distancia', index=['tamanyo', 'distancia-trie'], columns='threshold')
+print(df2)
+df2.to_csv('CSV_FINAL_PRUEBA.csv')
