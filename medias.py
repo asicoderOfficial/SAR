83c1277afd1b03fd1283ef09223fa30b3255a9cb
asicoderofficial@gmail.com <asicoderofficial@gmail.com>
2021-11-24 17:33:43 +0100

    yasta bien el csv

--- medias.py
+++ medias.py
@@ -1,10 +1,12 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('tiempos1.csv')
-df['media_trie-distancia'] = df.groupby(['trie', 'distancia', 'tamanyo'])['tiempo'].transform('mean')
-df['mediana_trie-distancia'] = df.groupby(['trie', 'distancia', 'tamanyo'])['tiempo'].transform('median')
+df['media_trie-distancia'] = df.groupby(['trie', 'distancia', 'tamanyo', 'threshold'])['tiempo'].transform('mean')
+df['mediana_trie-distancia'] = df.groupby(['trie', 'distancia', 'tamanyo', 'threshold'])['tiempo'].transform('median')
 df['distancia'] = df['distancia'].str.replace('intermediate','i')
 df['distancia'] = df['distancia'].str.replace('restricted','d')
 df['distancia'] = df['distancia'].str.replace('levenshtein','l')
-print(df)
-df.to_csv('tiempos_stats3.csv')
+df.to_csv('tiempos_stats4.csv')
